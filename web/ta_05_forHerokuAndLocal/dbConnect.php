63075c62ac8f9017ab7706414feb06c3a9a55641
Scott Burton <scotthburton@gmail.com>
2017-01-30 10:01:21 -0700

    TA 05 Solution

---
+++ web/ta_05_forHerokuAndLocal/dbConnect.php
@@ -0,0 +1,44 @@
+<?php
+/**********************************************************
+* File: dbConnect.php
+* Author: Br. Burton
+* 
+* Description: Shows how to connect using either local
+* OR Heroku credentials, depending on whether the code
+* is executing at heroku.
+***********************************************************/
+
+function get_db() {
+	$db = NULL;
+
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			// example localhost configuration URL with user: "ta_user", password: "ta_pass"
+			// and a database called "scripture_ta"
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
+		}
+
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+
+	return $db;
+}
---
+++ web/ta_05_forHerokuAndLocal/localCredentials.php
@@ -0,0 +1,18 @@
+<?php
+/**********************************************************
+* File: localCredentials.php
+* Author: Br. Burton
+* 
+* Description: I would keep this on my local computer
+* and NOT commit it to git (i.e., add it to my .gitignore file)
+* so that my username and password were keep private.
+***********************************************************/
+
+// It would be nice to additionally store this 
+$dbUser = 'ta_user';
+$dbPassword = 'ta_pass';
+$dbName = 'scripture_ta';
+$dbHost = 'localhost';
+$dbPort = '5432';
+
+?>
